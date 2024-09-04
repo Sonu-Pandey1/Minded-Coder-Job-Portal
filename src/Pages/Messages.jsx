3aba161948f5e5aba71ddd90b29eab3b808e1b1a
Sonu-Pandey1 <sonupandeybaldeo2003@gmail.com>
2024-09-04 21:31:55 +0530

    bookmark done

--- src/Pages/Messages.jsx
+++ src/Pages/Messages.jsx
@@ -2,8 +2,16 @@ import "./Messages.scss"
 
 function Messages() {
   return (
-    <main className="main-container">
-    <h1>messages</h1>
+    <main className="message-container">
+    <div className="emailVerifaction d-flex px-3 py-1 rounded-3  align-items-center">
+          <div>
+            <h5 className="pt-2">Email Verification Required</h5>
+            <p>Please verify your email address by clicking the link in the email we sent you. Please check your spam folder if you don't see the email. If you still don't see the email, please contact us.</p>
+          </div>
+          <div className="ms-auto ">
+            <button className="btn btn-outline-danger">Resend Email</button>
+          </div>
+        </div>
 
     </main>
   )

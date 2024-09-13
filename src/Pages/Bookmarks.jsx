158ba8d0b83a91698d8724def783356087c6db61
Sonu-Pandey1 <sonupandeybaldeo2003@gmail.com>
2024-09-13 11:50:05 +0530

    b done

--- src/Pages/Bookmarks.jsx
+++ src/Pages/Bookmarks.jsx
@@ -27,7 +27,7 @@ function Bookmarks() {
                         <p > Note: sonu apndey</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-info rounded-pill">Resume</p>
+                        <p className="badge text-dark bg-info-subtle p-2">Resume</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -38,7 +38,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -49,7 +49,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -60,7 +60,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -71,7 +71,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -82,7 +82,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -93,7 +93,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -104,7 +104,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -115,7 +115,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -126,7 +126,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -137,7 +137,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -148,7 +148,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -159,7 +159,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -170,7 +170,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
@@ -181,7 +181,7 @@ function Bookmarks() {
                         <p> Note: 3 persons</p>
                       </div>
                       <div className="ms-auto ">
-                        <p className="badge text-bg-warning rounded-pill">Job</p>
+                        <p className="badge text-dark bg-danger-subtle p-2">Job</p>
                         <button className="" ><svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" className="text-2xl text-themeLight hover:text-red-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z" fill="currentColor"></path><path d="M9 9H11V17H9V9Z" fill="currentColor"></path><path d="M13 9H15V17H13V9Z" fill="currentColor"></path></svg></button>
                       </div>
                     </li>
--- src/Pages/Messages.jsx
+++ src/Pages/Messages.jsx
@@ -1,6 +1,9 @@
 import "./Messages.scss"
 import EmailVerification from "../Components/EmailVerifaction"
 import Sidebar from "./Sidebar"
+import { Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Messages() {
   return (
@@ -12,9 +15,37 @@ function Messages() {
           </div>
         </div>
         <div className="col-10">
-          <main className="message-container py-5 px-5">
+          <main className="message-container py-5 px-5 ">
             <EmailVerification />
+            <div className="message mt-5">
+              <Container fluid className="bg-white">
+                <Row className="h-100 justify-content-between ">
+                  {/* Messages Section */}
+                  <Col md={7} className="d-flex align-items-center justify-content-center shadow" style={{ backgroundColor: 'white', borderRadius: '8px', border: '1px solid #e5e5e5' }}>
+                    <div className=" text-center">
+                      <p style={{ color: '#c0c0c0', fontSize: '1.2rem' }}>No Messages</p>
+                    </div>
+                  </Col>
 
+                  {/* Users Section */}
+                  <Col md={4} className="d-flex flex-column shadow" style={{ backgroundColor: 'white', borderRadius: '8px', border: '1px solid #e5e5e5', marginLeft: '15px' }}>
+                    <InputGroup className="my-4 ">
+                      <InputGroup.Text><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></InputGroup.Text>
+                      <FormControl
+                        className=""
+                        placeholder="Search"
+                        aria-label="Search"
+                        aria-describedby="search-bar"
+                      />
+                    </InputGroup>
+                    <div className="text-center" >
+                      <p style={{ color: '#c0c0c0', fontSize: '1.2rem' }}>No Users</p>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+
+            </div>
           </main>
         </div>
       </div>

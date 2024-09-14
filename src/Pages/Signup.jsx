4190d3c8ff09be98d376b672140f53f9f0dc35ea
Sonu-Pandey1 <sonupandeybaldeo2003@gmail.com>
2024-09-14 09:29:16 +0530

    SignUp from Valadition Done

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,40 +1,61 @@
+
 import { useEffect, useState } from 'react';
-import './Signup.scss';
-import { useFormik } from "formik"
+import { useFormik } from 'formik';
 import { SignupSchema } from '../Schemas/SignUpSchema';
+import './Signup.scss';
 
-let initialValues = {
-  username: "",
-  name: "",
-  user: "",
-  email: "",
-  e_email: "",
-  password: "",
-  e_password: "",
-  designation: "",
-  companyname: "",
-  c_password: "",
-  e_c_password: "",
-
-
-}
-
-// eslint-disable-next-line react/prop-types
 function SignUp({ closeModal }) {
+  const [selectedCategory, setSelectedCategory] = useState('candidate');
+  const [initialValues, setInitialValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    Cpassword: "",
+    name: "",
+    Eemail: "",
+    designation: "",
+    companyname: "",
+    Epassword: "",
+    ECpassword: "",
+    selectedCategory: 'candidate'
+  });
 
+  // Update initialValues based on selectedCategory
+  useEffect(() => {
+    if (selectedCategory === 'candidate') {
+      setInitialValues({
+        username: "",
+        email: "",
+        password: "",
+        Cpassword: "",
+        selectedCategory: 'candidate'
+      });
+    } else {
+      setInitialValues({
+        name: "",
+        Eemail: "",
+        designation: "",
+        companyname: "",
+        Epassword: "",
+        ECpassword: "",
+        selectedCategory: 'employer'
+      });
+    }
+  }, [selectedCategory]);
 
-
-  let { errors, values, touched, handleSubmit, handleChange, handleBlur } = useFormik({
-    initialValues: initialValues,
+  // Form Handling & Valdidation
+  const formik = useFormik({
+    initialValues,
     onSubmit: (values) => {
-      console.log(values)
+      console.log(values);
     },
-    validationSchema: SignupSchema
-  })
+    validationSchema: SignupSchema,
+    enableReinitialize: true,
+  });
 
+  // Close Module
   useEffect(() => {
     const handleClickOutside = (event) => {
-
       if (event.target.classList.contains('sign-up-modal')) {
         closeModal();
       }
@@ -44,124 +65,221 @@ function SignUp({ closeModal }) {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-
   }, [closeModal]);
 
-  const [selectedCategory, setSelectedCategory] = useState('candidate');
-  console.log(selectedCategory)
-
-  const handleWrapperClick = (event) => {
-    const inputElement = event.currentTarget.querySelector('input');
-    if (inputElement) {
-      inputElement.click();
-      setSelectedCategory(inputElement.id);
-    }
+  // Handle Select User
+  const handleUserTypeClick = (event) => {
+    const id = event.currentTarget.querySelector('input').id;
+    setSelectedCategory(id);
   };
-
 
   return (
     <div className="signup-wrapper">
       <div className="sign-up-modal">
         <div className="sign-up-content">
+
           <div className="wrapper d-flex justify-content-between align-items-center">
             <button className="close-btn p-2 rounded" onClick={closeModal}>✖</button>
-            <h2 >Create an Account</h2>
+            <h2>Create an Account</h2>
           </div>
-          <div className="select-category d-flex justify-content-center mt-5 mb-3 gap-5 ">
+
+          <div className="select-category d-flex justify-content-center mt-5 mb-3 gap-5">
             <div
               className={`category-wrapper ${selectedCategory === 'candidate' ? 'selected' : ''}`}
-              onClick={handleWrapperClick}
-            >
-              <label htmlFor="candidate1">Candidate</label>
-              <input type="radio" name="user" value={values.user} onChange={handleChange} onBlur={handleBlur} id="candidate" />
+              onClick={handleUserTypeClick} >
+              <label htmlFor="candidate">Candidate</label>
+              <input type="radio" name="user" id="candidate" />
             </div>
             <div
               className={`category-wrapper ${selectedCategory === 'employer' ? 'selected' : ''}`}
-              onClick={handleWrapperClick}
-            >
-              <label htmlFor="candidate2">Employer</label>
-              <input type="radio" name="user" value={values.user} onChange={handleChange} onBlur={handleBlur} id="employer" />
+              onClick={handleUserTypeClick} >
+              <label htmlFor="employer">Employer</label>
+              <input type="radio" name="user" id="employer" />
             </div>
           </div>
-          <form onSubmit={handleSubmit}>
-            {selectedCategory == "employer" ?
+
+          {/* User Form */}
+          <form onSubmit={formik.handleSubmit}>
+
+            {selectedCategory === "employer" ? (
               <>
                 <label htmlFor="name">Name</label>
-                <input type="text" autoFocus id='name' name='name' placeholder="Name" value={values.name} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.name}</p>
-              </> :
+                <input
+                  type="text"
+                  id='name'
+                  name='name'
+                  placeholder="Name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.name && formik.touched.name && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.name}</p>
+                )}
+              </>
+            ) : (
               <>
                 <label htmlFor="username">User Name</label>
-                <input type="text" autoFocus id='username' name='username' placeholder="Name" value={values.username} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.username}</p>
+                <input
+                  type="text"
+                  id='username'
+                  name='username'
+                  placeholder="User Name"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.username && formik.touched.username && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.username}</p>
+                )}
               </>
-            }
+            )}
 
-            {selectedCategory == "employer" ?
+            {selectedCategory === "employer" ? (
               <>
-                <label htmlFor="e_email">Email</label>
-                <input type="email" placeholder="Email" id='e_email' name='e_email' value={values.e_email} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.email}</p>
-              </> :
+                <label htmlFor="Eemail">Email</label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  id='Eemail'
+                  name='Eemail'
+                  value={formik.values.Eemail}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.Eemail && formik.touched.Eemail && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.Eemail}</p>
+                )}
+              </>
+            ) : (
               <>
                 <label htmlFor="email">Email</label>
-                <input type="email" placeholder="Email" id='email' name='email' value={values.email} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.e_email}</p>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  id='email'
+                  name='email'
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.email && formik.touched.email && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.email}</p>
+                )}
               </>
-            }
+            )}
 
-            {
-              selectedCategory == "employer" ? <><label htmlFor="Designation" className='mb-2'>Designation</label>
-                <select name='designation' value={values.designation} onChange={handleChange} onBlur={handleBlur}>
+            {selectedCategory === "employer" && (
+              <>
+                <label htmlFor="designation" className='mb-2'>Designation</label>
+                <select
+                  name='designation'
+                  value={formik.values.designation}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                >
                   <option value="">Choose Your Designation</option>
                   <option value="hr">HR</option>
                   <option value="manager">Manager</option>
                   <option value="employee">Employee</option>
                   <option value="seniorDeveloper">Senior Developer</option>
                 </select>
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.designation}</p>
-              </> : null
-            }
-            {
-              selectedCategory == "employer" ? <><label htmlFor="company-name">Comapny`s name</label>
-                <input type="text" placeholder="company-name" id='company-name' name='companyname' value={values.companyname} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.companyname}</p>
-                </> : null
-            }
+                {formik.errors.designation && formik.touched.designation && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.designation}</p>
+                )}
+              </>
+            )}
 
-            {selectedCategory == "employer" ?
+            {selectedCategory === "employer" && (
               <>
-                <label htmlFor="e_password">Password</label>
-                <input type="password" id='e_password' name='e_password' placeholder="Password" value={values.e_password} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.e_password}</p>
-              </> :
+                <label htmlFor="companyname">Company's Name</label>
+                <input
+                  type="text"
+                  placeholder="Company Name"
+                  id='companyname'
+                  name='companyname'
+                  value={formik.values.companyname}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.companyname && formik.touched.companyname && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.companyname}</p>
+                )}
+              </>
+            )}
+
+            {selectedCategory === "employer" ? (
+              <>
+                <label htmlFor="Epassword">Password</label>
+                <input
+                  type="password"
+                  id='Epassword'
+                  name='Epassword'
+                  placeholder="Password"
+                  value={formik.values.Epassword}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.Epassword && formik.touched.Epassword && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.Epassword}</p>
+                )}
+                <label htmlFor="ECpassword">Confirm Password</label>
+                <input
+                  type="password"
+                  id="ECpassword"
+                  name='ECpassword'
+                  placeholder="Confirm Password"
+                  value={formik.values.ECpassword}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.ECpassword && formik.touched.ECpassword && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.ECpassword}</p>
+                )}
+              </>
+            ) : (
               <>
                 <label htmlFor="password">Password</label>
-                <input type="password" id='password' name='password' placeholder="Password" value={values.password} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.password}</p>
+                <input
+                  type="password"
+                  id='password'
+                  name='password'
+                  placeholder="Password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.password && formik.touched.password && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.password}</p>
+                )}
+                <label htmlFor="Cpassword">Confirm Password</label>
+                <input
+                  type="password"
+                  id="Cpassword"
+                  name='Cpassword'
+                  placeholder="Confirm Password"
+                  value={formik.values.Cpassword}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.Cpassword && formik.touched.Cpassword && (
+                  <p className='form-error text-danger badge bg-danger-subtle'>{formik.errors.Cpassword}</p>
+                )}
               </>
+            )}
+
+            {selectedCategory === "employer" ?
+              <button type='submit' className="submit-btn">Submit</button> :
+              <button type='submit' className="submit-btn">Register</button>
             }
 
-            {selectedCategory == "employer" ?
-              <>
-                <label htmlFor="e_c_password">Confirm Password</label>
-                <input type="password" id="e_c_password" name='e_c_password' placeholder="Confirm_Password" value={values.e_c_password} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.e_c_password}</p>
-              </> :
-              <>
-                <label htmlFor="c_password">Confirm Password</label>
-                <input type="password" id="c_password" name='c_password' placeholder="Confirm_Password" value={values.c_password} onChange={handleChange} onBlur={handleBlur} required />
-                <p className='form-error text-danger badge bg-danger-subtle'>{errors.c_password}</p>
-              </>
-            }
-
-            <button type='submit' className="submit-btn">Submit</button>
           </form>
 
           <p className="login-prompt">
             Already have an account?
             <button className='btnn' type="button" onClick={closeModal}>Log In</button>
           </p>
+
         </div>
       </div>
     </div>
--- src/Schemas/SignUpSchema.jsx
+++ src/Schemas/SignUpSchema.jsx
@@ -1,16 +1,70 @@
-import * as Yup from "yup"
+
+
+
+import * as Yup from 'yup';
+
+let regexPass = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
 
 export const SignupSchema = Yup.object({
-    name: Yup.string().min(2).max(25).required("Please Enter Your name"),
-    username: Yup.string().min(2).max(25).required("Please Enter Your User Name"),
-    user: Yup.string().required("Please Select Your Type"),
-    email: Yup.string().email().required("Please Enter Your Email"),
-    e_email: Yup.string().email().required("Please Enter Your Email"),
-    password: Yup.string().min(6).required("Please Enter Your Password"),
-    e_password: Yup.string().min(8).required("Please Enter Your Password"),
-    designation: Yup.string().required("Please Select Your Designation"),
-    companyname: Yup.string().min(1).max(35).required("Please Enter Your Company Name"),
-    c_password: Yup.string().oneOf([Yup.ref("password"), null], "Please Must Match"),
-    e_c_password: Yup.string().oneOf([Yup.ref("password"), null], "Please Must Match"),
+    selectedCategory: Yup.string().required('Please select a category'),
 
+    name: Yup.string().when('selectedCategory', {
+        is: 'employer',
+        then: () => Yup.string().min(2).max(25).required("Please Enter Your Name"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    username: Yup.string().when('selectedCategory', {
+        is: 'candidate',
+        then: () => Yup.string().min(2).max(25).required("Please Enter Your User Name"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    email: Yup.string().when('selectedCategory', {
+        is: 'candidate',
+        then: () => Yup.string().email().required("Please Enter Your Email"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    Eemail: Yup.string().when('selectedCategory', {
+        is: 'employer',
+        then: () => Yup.string().email().required("Please Enter Your Email"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    password: Yup.string().when('selectedCategory', {
+        is: 'candidate',
+        then: () => Yup.string().min(6).required("Password must be at least 6 characters long.").matches(regexPass, "Password must be include uppercase, lowercase, number, and symbol"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    Epassword: Yup.string().when('selectedCategory', {
+        is: 'employer',
+        then: () => Yup.string().min(8).required("Password must be at least 8 characters long").matches(regexPass, "Password must be include uppercase, lowercase, number, and symbol"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    Cpassword: Yup.string().when('selectedCategory', {
+        is: 'candidate',
+        then: () => Yup.string().oneOf([Yup.ref('password'), null], "Passwords must match").required("Please Confirm Your Password"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    ECpassword: Yup.string().when('selectedCategory', {
+        is: 'employer',
+        then: () => Yup.string().oneOf([Yup.ref('Epassword'), null], "Passwords must match").required("Please Confirm Your Password"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    designation: Yup.string().when('selectedCategory', {
+        is: 'employer',
+        then: () => Yup.string().required("Please Select Your Designation"),
+        otherwise: () => Yup.string().nullable(),
+    }),
+
+    companyname: Yup.string().when('selectedCategory', {
+        is: 'employer',
+        then: () => Yup.string().min(1).max(35).required("Please Enter Your Company Name"),
+        otherwise: () => Yup.string().nullable(),
+    }),
 });
